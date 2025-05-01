28044493642a96ecf094e07135ee3748c0143f72
davidinovargas <davidinovargas@gmail.com>
2025-05-01 14:20:23 -0300

    Add project files.

---
+++ ControleDeMedicamentos.ConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using GestaoDeEquipamentos.ConsoleApp.Compartilhado;
+using GestaoDeEquipamentos.ConsoleApp.Util;
+
+namespace ControleDeMedicamentos.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TelaPrincipal telaPrincipal = new TelaPrincipal();
+
+            while (true)
+            {
+                telaPrincipal.ApresentarMenuPrincipal();
+
+                ITelaCrud telaSelecionada = telaPrincipal.ObterTela();
+
+                char opcaoEscolhida = telaSelecionada.ApresentarMenu();
+
+                switch (opcaoEscolhida)
+                {
+                    case '1': telaSelecionada.CadastrarRegistro(); break;
+
+                    case '2': telaSelecionada.EditarRegistro(); break;
+
+                    case '3': telaSelecionada.ExcluirRegistro(); break;
+
+                    case '4': telaSelecionada.VisualizarRegistros(true); break;
+
+                    default: break;
+                }
+            }
+        }
+    }
+}
---
+++ ControleDeMedicamentos.ConsoleApp/Util/TelaPrincipal.cs
@@ -0,0 +1,42 @@
+﻿using GestaoDeEquipamentos.ConsoleApp.Compartilhado;
+
+namespace GestaoDeEquipamentos.ConsoleApp.Util;
+
+public class TelaPrincipal
+{
+    private char opcaoPrincipal;
+    private ContextoDados contexto;
+
+
+    public TelaPrincipal()
+    {
+        this.contexto = new ContextoDados(true);
+    }
+
+    public void ApresentarMenuPrincipal()
+    {
+        Console.Clear();
+
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine("|        Gestão de Medicamentos        |");
+        Console.WriteLine("----------------------------------------");
+
+        Console.WriteLine();
+
+        Console.WriteLine("1 - Controle de ");
+        Console.WriteLine("S - Sair");
+
+        Console.WriteLine();
+
+        Console.Write("Escolha uma das opções: ");
+        opcaoPrincipal = Console.ReadLine()[0];
+    }
+
+        public ITelaCrud ObterTela()
+    {
+        //if (opcaoPrincipal == '1')
+            //return new 
+
+        return null;
+}
+}
