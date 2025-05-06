df759e777d74eb39440ae34e080285cd38322b13
davidinovargas <davidinovargas@gmail.com>
2025-05-06 14:35:51 -0300

    Criado Validador de Entrada para os Modulos

---
+++ ControleDeMedicamentos.ConsoleApp/Util/ValidarEntradaNaoVazia.cs
@@ -0,0 +1,63 @@
+﻿using GestaoDeEquipamentos.ConsoleApp.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleDeMedicamentos.ConsoleApp.Util
+{
+    public static class ValidadorEntrada
+    {
+        public static string ValidarEntradaNaoVazia(string mensagem)
+        {
+            string entrada;
+            do
+            {
+                Console.Write(mensagem);
+                entrada = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(entrada))
+                {
+                    Notificador.ExibirMensagem("A entrada não pode ser vazia. Tente novamente.", ConsoleColor.Red);
+                }
+
+            } while (string.IsNullOrWhiteSpace(entrada));
+
+            return entrada;
+        }
+        public static int ValidarInteiro(string mensagem)
+        {
+            int numero;
+            string entrada;
+            do
+            {
+                entrada = ValidarEntradaNaoVazia(mensagem);
+
+                if (!int.TryParse(entrada, out numero))
+                {
+                    Notificador.ExibirMensagem("Valor inválido. Digite um número inteiro válido.", ConsoleColor.Red);
+                }
+            } while (!int.TryParse(entrada, out numero));
+
+            return numero;
+        }
+
+        public static DateTime ValidarData(string mensagem)
+        {
+            DateTime data;
+            string entrada;
+            do
+            {
+                entrada = ValidarEntradaNaoVazia(mensagem);
+
+                if (!DateTime.TryParse(entrada, out data))
+                {
+                    Notificador.ExibirMensagem("Data inválida. Digite uma data no formato correto (dd/mm/aaaa).", ConsoleColor.Red);
+                }
+            } while (!DateTime.TryParse(entrada, out data));
+
+            return data;
+        }
+    }
+}
